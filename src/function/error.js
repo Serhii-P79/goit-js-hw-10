5faf4f4ce7583db2cee5cdc3a35b2cacd336dd62
Serhii Panasiuk <regidmail.sergej@gmail.com>
2021-11-16 04:07:01 +0200

    ++

--- src/function/error.js
+++ src/function/error.js
@@ -1,5 +1,10 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { ref } from './ref';
+
 export function error(errorID) {
+  ref.country_info.innerHTML = '';
+  ref.country_list.innerHTML = '';
+
   if (errorID === 404) {
     Notify.failure('Oops, there is no country with that name');
     return;
